c6d1d64e81437dea5fc60d9f8198c6867984f29b
EC2 Default User <ec2-user@ip-172-31-40-46.ap-northeast-1.compute.internal>
2023-02-19 13:59:04 +0000

    Add admin_customers

--- app/controllers/admin/customers_controller.rb
+++ app/controllers/admin/customers_controller.rb
@@ -1,12 +1,13 @@
 class Admin::CustomersController < ApplicationController
     def index
-      @customers = Customer.
+      @customers = Customer.page(params[:page])
     end
     
     def show
     end
     
     def edit
+      @customer = Customer.find(params[:id])
     end
     
     def update
--- config/routes.rb
+++ config/routes.rb
@@ -1,66 +1,4 @@
 Rails.application.routes.draw do
-<<<<<<< HEAD
- 
-
-  namespace :admin do
-
-
-     root to: 'homes#top'
-     resources :genres, only: [:index, :create, :edit, :update]
-     resources :items, only: [:index, :new, :create, :show, :edit, :update]
-     resources :customers, only: [:index, :show, :edit, :update]
-  
-
-  end
-
-
-
-
-
-  scope module: :public do
-    
-
-     resources :orders, only: [:new, :index, :create, :show]
-     post "orders/check"=>"orders#check",as:"check"
-     get "orders/over"=>"orders#over",as:"over"
-    
-     resources :cart_items, only: [:index, :update, :destroy, :create]
-     delete "cart_items/destroy_all"=>"cart_items#destroy_all",as:"destroy_all"
-     
-
-     get "customers/show" => "customers#show", as:"my_page"
-     get "customers/edit" => "customers#edit", as:"info_edit"
-     patch "customers/update" => "customers#update", as:"info"
-     get "customers/quit" => "customers#quit", as:"quite"
-     patch "customers/withdrawal" => "customers#withdrawal", as:"withdrawal"
-  
-    
-    
-    
-    
-    
-    
-    
-    
-
-
-  end
-
-
-
-
-
-  
-  
- 
-  
-  
-  
-  
-  
-  
-
-=======
   devise_for :customers
   devise_for :admins
 
@@ -68,11 +6,13 @@ Rails.application.routes.draw do
   get '/about' => 'home#about', as: 'about'
 
   namespace :admin do
+    root to: 'homes#top'
     resources :genres, only: [:index, :create, :edit, :update]
     resources :customers, only: [:index, :show, :edit, :update]
     resources :orders, only: [:show, :create]
     resources :order_details, only: [:update]
     resources :sessions, only: [:new, :create, :destroy]
+    resources :items, only: [:index, :new, :create, :show, :edit, :update]
   end
 
   scope module: :public do
@@ -83,9 +23,13 @@ Rails.application.routes.draw do
     delete "cart_items/destroy_all"=>"cart_items#destroy_all",as:"destroy_all"
     resources :items, only: [:index, :show]
     resources :addresses, only: [:index, :edit, :create, :update, :destroy]
+     get "customers/show" => "customers#show", as:"my_page"
+     get "customers/edit" => "customers#edit", as:"info_edit"
+     patch "customers/update" => "customers#update", as:"info"
+     get "customers/quit" => "customers#quit", as:"quite"
+     patch "customers/withdrawal" => "customers#withdrawal", as:"withdrawal"
   end
 
 
->>>>>>> origin/develop
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
