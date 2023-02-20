3e823e23cebbac10a21e60b1282bb3f033767b48
EC2 Default User <121584968+Shinji20221229@users.noreply.github.com>
2023-02-20 04:18:52 +0000

    commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,78 +1,19 @@
 Rails.application.routes.draw do
-<<<<<<< HEAD
- 
-
-  namespace :admin do
-
-
-     root to: 'homes#top'
-     resources :genres, only: [:index, :create, :edit, :update]
-     resources :items, only: [:index, :new, :create, :show, :edit, :update]
-     resources :customers, only: [:index, :show, :edit, :update]
-  
-
-  end
-
-
-
-
-
-  scope module: :public do
-    
-
-     resources :orders, only: [:new, :index, :create, :show]
-     post "orders/check"=>"orders#check",as:"check"
-     get "orders/over"=>"orders#over",as:"over"
-    
-     resources :cart_items, only: [:index, :update, :destroy, :create]
-     delete "cart_items/destroy_all"=>"cart_items#destroy_all",as:"destroy_all"
-     
-
-     get "customers/show" => "customers#show", as:"my_page"
-     get "customers/edit" => "customers#edit", as:"info_edit"
-     patch "customers/update" => "customers#update", as:"info"
-     get "customers/quit" => "customers#quit", as:"quite"
-     patch "customers/withdrawal" => "customers#withdrawal", as:"withdrawal"
-  
-    
-    
-    
-    
-    
-    
-    
-    
-
-
-  end
-
-
-
-
-
-  
-  
- 
-  
-  
-  
-  
-  
-  
-
-=======
   devise_for :customers
   devise_for :admins
 
-  root to: "homes#top"
-  get '/about' => 'home#about', as: 'about'
+  # root to: 'homes#top'
+  root to: "public/homes#top"
+  get '/about' => 'home#about'
 
   namespace :admin do
+    root to: "homes#top"
     resources :genres, only: [:index, :create, :edit, :update]
     resources :customers, only: [:index, :show, :edit, :update]
-    resources :orders, only: [:show, :create]
+    resources :orders, only: [:indes, :show, :create]
     resources :order_details, only: [:update]
     resources :sessions, only: [:new, :create, :destroy]
+    resources :items, only: [:index, :new, :create, :show, :edit, :update]
   end
 
   scope module: :public do
@@ -83,9 +24,12 @@ Rails.application.routes.draw do
     delete "cart_items/destroy_all"=>"cart_items#destroy_all",as:"destroy_all"
     resources :items, only: [:index, :show]
     resources :addresses, only: [:index, :edit, :create, :update, :destroy]
+    get "customers/show" => "customers#show", as:"my_page"
+    get "customers/edit" => "customers#edit", as:"info_edit"
+    patch "customers/update" => "customers#update", as:"info"
+    get "customers/unsubscribe" => "customers#unsubscribe", as:"unsubscribe"
+    patch "customers/withdraw" => "customers#withdraw", as:"withdraw"
   end
 
-
->>>>>>> origin/develop
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
