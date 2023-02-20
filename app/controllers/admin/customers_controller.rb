42c1ac7b349aa9cf01afda507eb20b1986ff2221
EC2 Default User <ec2-user@ip-172-31-40-46.ap-northeast-1.compute.internal>
2023-02-20 09:29:13 +0000

    Add merge adassan

--- app/controllers/admin/customers_controller.rb
+++ app/controllers/admin/customers_controller.rb
@@ -24,6 +24,6 @@ class Admin::CustomersController < ApplicationController
     
     private
       def customer_params
-        params.require(:customer).permit(:first_name,:last_name,:first_name_kana,:last_name_kana,:email,:postacode,:address,:telephone_number,:is_deleted)
+        params.require(:customer).permit(:first_name,:last_name,:first_name_kana,:last_name_kana,:email,:postcode,:address,:telephone_number,:is_deleted)
       end
 end
