6a2262fa17c6d157489204912f11d59884bd205b
EC2 Default User <ec2-user@ip-172-31-40-46.ap-northeast-1.compute.internal>
2023-02-20 12:21:45 +0000

    Add public_cart_item

--- app/controllers/public/cart_items_controller.rb
+++ app/controllers/public/cart_items_controller.rb
@@ -1,5 +1,7 @@
 class Public::CartItemsController < ApplicationController
     def index
+        @cart_items=current_customer,cart_items
+        @total_price =current_customer.cart_products.cart_products_total_price(@cart_products)
     end
     
     def update
