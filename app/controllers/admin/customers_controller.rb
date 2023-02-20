b16d5be226302a2c3316483702bf8aee619d65ce
EC2 Default User <ec2-user@ip-172-31-40-46.ap-northeast-1.compute.internal>
2023-02-20 09:17:40 +0000

    Add merge

--- app/controllers/admin/customers_controller.rb
+++ app/controllers/admin/customers_controller.rb
@@ -23,6 +23,7 @@ class Admin::CustomersController < ApplicationController
     end
     
     private
-    def customer_params
-      params.
+      def customer_params
+        params.require(:customer).permit(:first_name,:last_name,:first_name_kana,:last_name_kana,:email,:postacode,:address,:telephone_number,:is_deleted)
+      end
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,7 @@
 class Item < ApplicationRecord
+    
+    ## 消費税を求めるメソッド
+    def with_tax_price
+        (price * 1.1).floor
+    end
 end
