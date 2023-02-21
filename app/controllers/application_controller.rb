5b8abac70fcc9cc082c7e14ae3cf0e6c77a73b0f
TasakiFumiko <121810685+tasakifumiko@users.noreply.github.com>
2023-02-21 06:52:16 +0000

    to get merging

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,16 +1,4 @@
 class ApplicationController < ActionController::Base
-<<<<<<< HEAD
-  
-  before_action :configure_permitted_parameters, if: :devise_controller?
-
-  private
-
-  def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:last_name,:first_name,:last_name,:last_name_kana,:first_name_kana,:email,:encrypted_password,:postcode,:address,:phone_number])
-  end
-
-end
-=======
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   private
@@ -26,4 +14,3 @@ end
 
 
 
->>>>>>> origin/develop
