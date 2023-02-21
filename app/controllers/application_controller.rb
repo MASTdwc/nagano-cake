99eb4f2949df49ca2fd576adc66adea35f2cdb14
TasakiFumiko <121810685+tasakifumiko@users.noreply.github.com>
2023-02-21 06:10:54 +0000

    implemented admin/genre

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,11 @@
 class ApplicationController < ActionController::Base
+  
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  private
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:last_name,:first_name,:last_name,:last_name_kana,:first_name_kana,:email,:encrypted_password,:postcode,:address,:phone_number])
+  end
+
 end
