37d6beb1ea7db9e81e07ff4231c02d871f0638a3
EC2 Default User <ec2-user@ip-172-31-40-46.ap-northeast-1.compute.internal>
2023-02-21 01:16:37 +0000

    add Admin_cart_items contoroller

--- app/controllers/public/cart_items_controller.rb
+++ app/controllers/public/cart_items_controller.rb
@@ -1,10 +1,12 @@
 class Public::CartItemsController < ApplicationController
     def index
-        @cart_items=current_customer,cart_items
-        @total_price =current_customer.cart_products.cart_products_total_price(@cart_products)
+      @cart_items=current_customer,cart_items
+      @total_price =current_customer.cart_products.cart_products_total_price(@cart_products)
     end
     
     def update
+      @cacrt_item = CartItem.find(params[:id])
+      if params[:cart_item]
     end
     
     def destroy
