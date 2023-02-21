53a66e56ddad84b4ca992f6b9d7e193fe5571739
TasakiFumiko <121810685+tasakifumiko@users.noreply.github.com>
2023-02-21 09:37:35 +0000

    login実装

--- app/controllers/public/sessions_controller.rb
+++ app/controllers/public/sessions_controller.rb
@@ -39,10 +39,14 @@ protected
   def reject_customer
    @customer = Customer.find_by(email: params[:customer][:email])
    return if !@customer
+   puts 'test1'
+   
    if @customer.valid_password?(params[:customer][:password]) && @customer.is_customer_deleted
      flash[:notice] = "退会済みです。再度ご登録をお願い致します。"
+     puts 'test2'
      redirect_to new_customer_session_path
    else
+     puts 'test3'
      customer_session_path
    end
   end
