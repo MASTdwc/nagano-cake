b1d159326c416a7b6c688166ef3b14138afba0f6
EC2 Default User <121584968+Shinji20221229@users.noreply.github.com>
2023-02-21 07:49:54 +0000

    commit

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -3,14 +3,7 @@ class Customer < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-<<<<<<< HEAD
 
-  has_many :addresses
-  has_many :cart_items
-  has_many :orders
-
-=======
-  
   validates :last_name, length: { in: 2..20 },
                    uniqueness: true
   validates :first_name, length: { in: 2..20 },
@@ -20,7 +13,11 @@ class Customer < ApplicationRecord
   validates :email, presence: true, format: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :postcode, presence: true, format: /\A\d{7}\z/
   validates :address, presence: true
-  validates :phone_number, presence: true, format: /\A\d{10,11}\z/       
->>>>>>> origin/develop
+  validates :phone_number, presence: true, format: /\A\d{10,11}\z/
+
+  has_many :addresses
+  has_many :cart_items
+  has_many :orders
+
 end
 
