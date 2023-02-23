6163abed5c84ab71b29829cba05e420c7a25bd39
EC2 Default User <121584968+Shinji20221229@users.noreply.github.com>
2023-02-23 14:28:31 +0000

    commit

--- app/controllers/admin/order_details_controller.rb
+++ app/controllers/admin/order_details_controller.rb
@@ -1,20 +1,29 @@
 class Admin::OrderDetailsController < ApplicationController
 
   def update
+  #   @order_detail = OrderDetail.find(params[:id])
+  #   @order_detail = @order_detail
+  #   @order_detail.update(order_detail_params)
+  #   if @order_detail.where(making_status: 2).count >= 1
+  #     @order.status = 2
+  #     @order.save
+  #   end
+
+  #   if @order_details.where(product_status: 3).count == @order_details.count
+  #     @order.status = 3
+  #     @order.save
+  #   end
+  #   redirect_to admin_order_path(@order)
+  # end
     # @order = Order.find(params[:order_id])
-    @order_detail = OrderDetail.find(params[:id])
-    @order_detail = @order_details
-    @order_detail.update(order_details_params)
-    if @order_details.where(product_status: 2).count >=1
-      @order.status = 2
-      @order.save
+    @order = OrderDetail.find(params[:id])
+    if @order.update(order_detail_params)
+      flash[:notice] = "successfully"
+      redirect_to admin_order_path(@order)
+    else
+      admin_order_path(@order)
     end
 
-    if @order_details.where(product_status: 3).count == @order_details.count
-      @order.status = 3
-      @order.save
-    end
-    redirect_to admin_order_path(@order)
   end
 
   private
